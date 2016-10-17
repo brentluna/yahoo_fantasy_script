ec04b5def6cfe6c8a743ec68641e9ff010f56487
Brent Luna <brentadamluna@gmail.com>
2016-10-17 12:33:40 -0700

    bookmarklets won't work as it will only work on current page and won't handle page refresh

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-const nextButton = document.getElementsByClassName('Js-next Grid-u No-bdr-radius-start No-bdrstart Pstart-med Td-n Fz-xs');
-console.log(nextButton);
-nextButton.click();
+//const startActive = document.getElementsByClassName('Btn Btn-short Mend-med')[3];
+
+const clickInt = setInterval(() => {
+	const nextButton = document.getElementsByClassName('Js-next Grid-u No-bdr-radius-start No-bdrstart Pstart-med Td-n Fz-xs')[0];
+	nextButton.click();
+}, 1000);
